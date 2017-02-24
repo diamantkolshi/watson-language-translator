65bbf552ea48f7be67ac0b8b9d8111f749eb91ed
Diamant <diamantkolshi@gmail.com>
2017-02-24 18:23:46 +0100

    add request module

--- lib/watson_language/generator/language_request.rb
+++ lib/watson_language/generator/language_request.rb
@@ -5,10 +5,18 @@ module WatsonLanguage
         "#{WatsonLanguage.base_url}"
       end
 
-      def rest_client_api(request_api)
+      def rest_client_api(request_api_url)
+        RestClient::Request.execute(
+          method: :get,
+          url: request_api_url,
+          user: @username,
+          password: @password,
+          :headers => {:accept => :json}
+        )
       end
 
       def json_parser(url)
+        JSON.parse(rest_client_api(base_url_request + "/" + url))
       end
     end
   end
