85042b82931df16beb59a1234a0497ee1d8a6a65
Diamant <diamantkolshi@gmail.com>
2017-02-24 18:26:59 +0100

    add translator class with endpoint method and add response field

--- lib/watson_language/translator.rb
+++ lib/watson_language/translator.rb
@@ -1,7 +1,12 @@
 module WatsonLanguage
   class Translator < ActiveMethod::Base
+    add_response_field :translations
+    add_response_field :word_count
+    add_response_field :character_count
+
     def endpoint
       url = "translate"
+      url << "?text=#{@text}"
       url << "&#{params_addressable}" unless params_addressable.empty?
       url
     end
