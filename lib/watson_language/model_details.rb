90ec5f27245ed1b57e8ccf0058622e281677c2a8
Diamant <diamantkolshi@gmail.com>
2017-02-27 22:03:11 +0100

    add model_details class, rspec test and json file

--- lib/watson_language/model_details.rb
+++ lib/watson_language/model_details.rb
@@ -1,7 +1,13 @@
 module WatsonLanguage
   class ModelDetails < ActiveMethod::Base
+  	add_response_field :models
+  	
+    def self.fetch(options={})
+      self.new(nil, options)
+    end
+
     def endpoint
-    	"models/#{@options[:model_id]}"
+      "models/#{@options[:model_id]}"
     end
   end
 end
