d594608cf2963a9beab12a5bd68810a207f160de
Diamant <diamantkolshi@gmail.com>
2017-02-28 11:02:09 +0100

    modify request class module

--- lib/watson_language/generator/language_request.rb
+++ lib/watson_language/generator/language_request.rb
@@ -7,7 +7,7 @@ module WatsonLanguage
 
       def rest_client_api(request_api_url)
         RestClient::Request.execute(
-          method: :get,
+          method: @http_method.to_sym,
           url: request_api_url,
           user: @username,
           password: @password,
