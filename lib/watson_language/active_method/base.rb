ad3388d6eb44d6eacfac8a569a6781975f91afde
Diamant <diamantkolshi@gmail.com>
2017-02-28 11:00:20 +0100

    add check method for http requset method

--- lib/watson_language/active_method/base.rb
+++ lib/watson_language/active_method/base.rb
@@ -2,13 +2,14 @@ module WatsonLanguage
   module ActiveMethod
     class Base < ActiveMethod::Extra
       include WatsonLanguage::Generators::LanguageRequest
-      attr_accessor :text, :username, :password, :json_result, :options
+      attr_accessor :text, :username, :password, :json_result, :options, :http_method
 
       def initialize(text = nil, options={})
         @text = text
         @username = WatsonLanguage.username
         @password = WatsonLanguage.password
         @options = options
+        @http_method = fetch_http_method
         @json_result = request
       end
 
@@ -20,6 +21,16 @@ module WatsonLanguage
         uri = Addressable::URI.new
         uri.query_values = @options
         uri.query
+      end
+
+      def fetch_http_method
+        if @options[:http_method]
+          result = options[:http_method]
+          @options.delete(:http_method)
+        else
+          result = "get"
+        end
+        return result.to_sym
       end
     end
   end
