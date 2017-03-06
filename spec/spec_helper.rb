98ab974cafe84ae298b165752ab0b86ff728a7b1
Diamant <diamantkolshi@gmail.com>
2017-03-06 10:39:35 +0100

    change name of spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), 'lib/*.rb', '..'))
-$: << File.join(APP_ROOT, 'lib/watson_language')
+$: << File.join(APP_ROOT, 'lib/watson-language-translator')
 
 require 'rest-client'
 require 'json'
 require 'pry'
-require 'watson_language'
+require 'watson-language-translator'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
